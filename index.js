f2b5864e990b1ba9eead47e86bd2ed78733195d7
Siddharth Raj <lastsraj@gmail.com>
2020-10-30 01:06:18 +0530

    Update to Mapbox GL JS, light theme and other enhancements

--- index.js
+++ index.js
@@ -4,52 +4,73 @@ let speedText = document.getElementById('speed');
 let timeText = document.getElementById('time');
 let altitudeText = document.getElementById('altitude');
 let visibilityText = document.getElementById('visibility');
-let lat = 51.505;
-let long = -0.09;
-let zoomLevel = 2;
+let lat = 48.505;
+let long = 2.09;
+let zoomLevel = 4;
 
-const icon = L.icon({
-    iconUrl: './space2.png',
-    iconSize: [80, 60],
-    iconAnchor: [25, 94],
-    popupAnchor: [20, -86]
-  });
+let json1 = {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [0.0, 10.1]
+    },
+    "properties": {
+      "name": "Dinagat Islands"
+    }
+  };
 
-const map = L.map('map-div',  { attributionControl:false }).setView([lat, long], zoomLevel);
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2lkZGhhcnRocmFqIiwiYSI6ImNrZ3A2bDR6ejA3NmsyeHRkc2lwb3h5Nm4ifQ.RchD-9rj9iRPiHgO9w_ZEA';
+var map = new mapboxgl.Map({
+container: 'map-div',
+style: 'mapbox://styles/mapbox/dark-v10',
+center: [lat, long],
+zoom: 5
+});
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 30,
-  id: 'siddharthraj/ckgp7gke825el19odvceegd3o',
-  tileSize: 512,
-  zoomOffset: -1,
-  noWrap: false,
 
-  accessToken: 'pk.eyJ1Ijoic2lkZGhhcnRocmFqIiwiYSI6ImNrZ3A2bDR6ejA3NmsyeHRkc2lwb3h5Nm4ifQ.RchD-9rj9iRPiHgO9w_ZEA'
-}).addTo(map);
+var url = 'https://api.wheretheiss.at/v1/satellites/25544';
+map.on('load', function () {
+var request = new XMLHttpRequest();
+window.setInterval(function () {
+// make a GET request to parse the GeoJSON at the url
+request.open('GET', url, true);
+request.onload = function () {
+if (this.status >= 200 && this.status < 400) {
+// retrieve the JSON from the response
+var character = JSON.parse(this.response);
+lat = character.longitude.toFixed(2);
+long = character.latitude.toFixed(2);
+latText.innerHTML = `<p> ${character.latitude.toFixed(2)} </p>`
+longText.innerHTML = `<p> ${character.longitude.toFixed(2)} </p>`
+timeText.innerHTML = `<p>${new Date(character.timestamp * 1000).toUTCString()}</p>`
+altitudeText.innerHTML = `<p> ${character.altitude.toFixed(2)} </p>`
+visibilityText.innerHTML = `<p> ${character.visibility} </p>`
+speedText.innerHTML = `<p> ${character.velocity.toFixed(2)} km/hr</p>`
+var json1 = {"geometry": {"type": "Point", "coordinates": [149.3293629818572, -49.10781621585451]}, "type": "Feature", "properties": {}};
 
-const marker = L.marker([lat, long], { icon: icon }).addTo(map);
-
-function updateInfo() {
-
-    fetch('https://api.wheretheiss.at/v1/satellites/25544')
-    .then(res => res.json())
-    .then(character => {
-        
-        lat = character.longitude.toFixed(2);
-        long = character.latitude.toFixed(2);
-        latText.innerHTML = `<p> ${character.latitude.toFixed(2)} </p>`
-        longText.innerHTML = `<p> ${character.longitude.toFixed(2)} </p>`
-        timeText.innerHTML = `<p>${new Date(character.timestamp * 1000).toUTCString()}</p>`
-        altitudeText.innerHTML = `<p> ${character.altitude.toFixed(2)} </p>`
-        visibilityText.innerHTML = `<p> ${character.visibility} </p>`
-        speedText.innerHTML = `<p> ${character.velocity.toFixed(2)} km/hr</p>`
-
-        marker.setLatLng([lat, long]);
-        map.setView([lat, long]);
-    })
+json1.geometry.coordinates = [lat,long]
+// update the drone symbol's location on the map
+map.getSource('drone').setData(json1);
+ 
+// fly the map to the drone's current location
+map.flyTo({
+center: json1.geometry.coordinates,
+speed: 0.5
+});
 }
+};
+request.send();
+}, 3000);
+ 
+map.addSource('drone', { type: 'geojson', data: {"geometry": {"type": "Point", "coordinates": [48.3293629818572, 2.10781621585451]}, "type": "Feature", "properties": {}}
+});
+map.addLayer({
+'id': 'drone',
+'type': 'symbol',
+'source': 'drone',
+'layout': {
+'icon-image': 'rocket-15'
+}
+});
+});
 
-updateInfo();
-
-setInterval(function(){ updateInfo(); }, 3000);
